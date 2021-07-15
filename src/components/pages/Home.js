6cc3c86e9fb5796600e67e12b84c96b094d9d98e
reyhantamang@gmail.com <reyhantamang@gmail.com>
2021-07-15 12:21:42 +0530

    modifed

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,11 @@
 import "../../App.css"
 import HeroSection from "../HeroSection"
-
+import Cards from "../Cards"
 export function Home() {
   return (
     <>
       <HeroSection />
+      <Cards />
     </>
   )
 }
