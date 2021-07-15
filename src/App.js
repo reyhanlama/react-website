bf1bacf0d39fc805f30c739560c834ff0fe53a9f
reyhantamang@gmail.com <reyhantamang@gmail.com>
2021-07-15 13:18:19 +0530

    changed to gangtok travel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import './App.css';
-import Home from './components/pages/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Services from './components/pages/Services';
-import Products from './components/pages/Products';
-import SignUp from './components/pages/SignUp';
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Home from "./components/pages/Home"
+
+import Services from "./components/pages/Services"
+import Products from "./components/pages/Products"
+import SignUp from "./components/pages/SignUp"
+import "./App.css"
 
 function App() {
   return (
@@ -13,14 +14,14 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} />
+          <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/products" component={Products} />
+          <Route path="/sign-up" component={SignUp} />
         </Switch>
       </Router>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,51 +1,57 @@
-import React from 'react';
-import './Cards.css';
-import CardItem from './CardItem';
+import React from "react"
+import "./Cards.css"
+import CardItem from "./CardItem"
 
 function Cards() {
   return (
-    <div className='cards'>
-      <h1>Check out these EPIC Destinations!</h1>
-      <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items'>
+    <div className="cards">
+      {/* <h1>Check out these EPIC Destinations!</h1> */}
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
             <CardItem
-              src='images/img-9.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
-              path='/services'
+              src="images/north.jpg"
+              text="Snowfall in Lachung"
+              label="Adventure"
+              path="/services"
             />
             <CardItem
-              src='images/img-2.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
-              path='/services'
+              src="images/zuluk.jpg"
+              text="The Ancient Silk Route"
+              label="Luxury"
+              path="/services"
+            />
+            <CardItem
+              src="images/lake.jpg"
+              text="One of the highest lakes in the world"
+              label="Adrenaline"
+              path="/sign-up"
             />
           </ul>
-          <ul className='cards__items'>
+          <ul className="cards__items">
             <CardItem
-              src='images/img-3.jpg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-              label='Mystery'
-              path='/services'
+              src="images/south.jpg"
+              text="Adventures in South Sikkim"
+              label="Mystery"
+              path="/services"
             />
             <CardItem
-              src='images/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
-              path='/products'
+              src="images/monk.jpg"
+              text="The Ancinet Monasteries of Sikkim"
+              label="Adventure"
+              path="/products"
             />
             <CardItem
-              src='images/img-8.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
-              path='/sign-up'
+              src="images/lake.jpg"
+              text="One of the highest lakes in the world"
+              label="Adrenaline"
+              path="/sign-up"
             />
           </ul>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Cards;
+export default Cards
